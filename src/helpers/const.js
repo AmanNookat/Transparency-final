680a13244bca976ccf4b9e0184846d96fbea4130
Aman <amantur.mendibaev2003@gmail.com>
2024-06-20 11:42:02 +0600

    a

--- src/helpers/const.js
+++ src/helpers/const.js
@@ -1,11 +1,11 @@
 export const USERS_API = "http://35.242.251.243/api/v1/account";
 
-export const TENDERS_API = "http://localhost:8000/tenders";
+export const TENDERS_API = "https://zakupki-json2.vercel.app/tenders";
 
-export const COMMENT_API = "http://localhost:8000/comments";
+export const COMMENT_API = "https://zakupki-json2.vercel.app/comments";
 
-export const REPORT_API = "http://localhost:8000/report";
+export const REPORT_API = "https://zakupki-json2.vercel.app/report";
 
-export const CIRCLE_API = "http://localhost:8000/info_circle";
+export const CIRCLE_API = "https://zakupki-json2.vercel.app/info_circle";
 
-export const CHARTS_API = "http://localhost:8000/charts";
+export const CHARTS_API = "https://zakupki-json2.vercel.app/charts";
